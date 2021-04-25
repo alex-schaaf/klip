da4240f91c24d5180d3b4a85d1e98e80a66da886
alex-schaaf <17277800+alex-schaaf@users.noreply.github.com>
2021-04-25 16:50:28 +0200

    formatting with black

--- klip.py
+++ klip.py
@@ -7,8 +7,8 @@ import src
 
 app = typer.Typer()
 
-username = os.path.expanduser('~').split('/')[-1]
-kindle_path = Path(f'/media/{username}/Kindle/')
+username = os.path.expanduser("~").split("/")[-1]
+kindle_path = Path(f"/media/{username}/Kindle/")
 
 
 def main(destination: str, verbose: bool = False) -> None:
@@ -16,24 +16,20 @@ def main(destination: str, verbose: bool = False) -> None:
     if not kindle_is_connected():
         return
 
-    clippings_file = kindle_path / 'documents/My Clippings.txt'
+    clippings_file = kindle_path / "documents/My Clippings.txt"
     if not os.path.isfile(clippings_file):
-        typer.echo('No clippings found on connected Kindle.')
+        typer.echo("No clippings found on connected Kindle.")
         return
 
     klipings_lines = src.read_clippings(clippings_file)
     clippings = src.sort_clippings(klipings_lines)
-    src.write_clippings(
-        clippings,
-        destination,
-        verbose=verbose
-    )
+    src.write_clippings(clippings, destination, verbose=verbose)
 
 
 def kindle_is_connected() -> bool:
     """Checks if Kindle device is connected. Also checks for OS, as
     app only works on Linux as of now."""
-    if platform != 'linux':
+    if platform != "linux":
         typer.echo(f"{platform} not supported. Current support is linux only.")
         return False
     return os.path.exists(kindle_path)
--- src/__init__.py
+++ src/__init__.py
@@ -3,16 +3,16 @@ from pathlib import Path
 import os
 
 
-def read_clippings(fp: str, encoding : str = "utf-8-sig") -> list:
+def read_clippings(fp: str, encoding: str = "utf-8-sig") -> list:
     """Read in Kindle clippings text file as bytes, convert to correct string
     format and strip formatting codes.
-    
+
     Args:
         fp (str): Filepath of the clippings text file.
-    
+
     Returns:
         (list) of lines as strings..
-    
+
     """
     with open(fp, "rb") as file:
         bytes_ = file.readlines()
@@ -22,25 +22,25 @@ def read_clippings(fp: str, encoding : str = "utf-8-sig") -> list:
 
 def slicer(iterable) -> slice:
     """Iteratively creates slice object from the given iterable containing
-    indices. 
-    
+    indices.
+
     Args:
         iterable: List or tuple of indices.
-    
+
     Yields:
         (slice)
     """
-    for i, j  in zip(iterable[:-1], iterable[1:]):
+    for i, j in zip(iterable[:-1], iterable[1:]):
         yield slice(i, j)
 
 
 def find(iterable, x) -> list:
     """Find all occurences of x in given iterable and return indices
-    
+
     Args:
         iterable: List of tuple in which to search.
         x: Element for which so search in given iterable.
-        
+
     Returns:
         (list) of indices.
     """
@@ -51,16 +51,16 @@ def find(iterable, x) -> list:
     return seps
 
 
-def sort_clippings(lines: list, seperator:str = '==========') -> dict:
+def sort_clippings(lines: list, seperator: str = "==========") -> dict:
     """Sort clippings in given list of lines clippings.
-    
+
     Args:
         lines(list): List of lines read from Kindle clippings txt file.
         separator(str, optional): Separator of individual highlights.
             Default: '=========='
-    
+
     Returns:
-        (dict)    
+        (dict)
     """
     lines.insert(0, seperator)
     sep = find(lines, seperator)
@@ -78,6 +78,7 @@ def sort_clippings(lines: list, seperator:str = '==========') -> dict:
         clippings[entry[1]]["highlights"].append(entry[-1])
         string = entry[2]
         import re
+
         # page pattern
         pattern = re.compile(r"page \d{1,}")
         try:
@@ -94,38 +95,44 @@ def sort_clippings(lines: list, seperator:str = '==========') -> dict:
         clippings[entry[1]]["loc"].append(loc)
         clippings[entry[1]]["page"].append(page)
         clippings[entry[1]]["time"].append(datetime)
-        
+
     return clippings
 
 
-def write_clippings(clippings: dict, path: str, encoding: str = "utf-8", verbose: bool = False) -> None:
+def write_clippings(
+    clippings: dict, path: str, encoding: str = "utf-8", verbose: bool = False
+) -> None:
     """Write clippings to markdown files at given location.
-    
+
     Args:
         clippings (dict): Dictionary containing clippings.
         path (str): Path of directory to write in.
         encoding (str, optional): String encoding. Default: utf-8
-        
+
     Returns:
         None
     """
     skip_counter = 0
     sync_counter = 0
     for key, value in clippings.items():
-        authorpath = Path(path) / value.get('author')
+        authorpath = Path(path) / value.get("author")
 
         # check if filepath exists, if not: mkdir
         if not os.path.isdir(authorpath):
             if verbose:
                 print(f"Creating folder for author {value.get('author')}.")
             os.makedirs(authorpath)
-        
+
         # clean filename of special characters
         # https://stackoverflow.com/questions/23996118/replace-special-characters-in-a-string-python#23996414
-        filename = value.get('title').translate(
-            {ord(c): " " for c in "!@#$%^&*()[]{};:,./<>?\|`~-=_+"}) + ".md"
+        filename = (
+            value.get("title").translate(
+                {ord(c): " " for c in "!@#$%^&*()[]{};:,./<>?\|`~-=_+"}
+            )
+            + ".md"
+        )
         filepath = authorpath / filename
-        
+
         # if file does not exist, create and write book header
         if not os.path.isfile(filepath):
             if verbose:
@@ -133,15 +140,15 @@ def write_clippings(clippings: dict, path: str, encoding: str = "utf-8", verbose
             with open(filepath, "wb") as file:
                 title = "# " + key + "\n\n"
                 file.write(title.encode(encoding))
-        
+
         # read existing file to check for duplicate clippings later
         content = filepath.read_text(encoding=encoding)
         with open(filepath, "a+b") as file:  # append mode
             for page, loc, time, text in zip(
-                value.get("page"), 
-                value.get("loc"), 
-                value.get("time"), 
-                value.get("highlights")
+                value.get("page"),
+                value.get("loc"),
+                value.get("time"),
+                value.get("highlights"),
             ):
                 # check if clipping already in file
                 if time in content:
@@ -149,7 +156,7 @@ def write_clippings(clippings: dict, path: str, encoding: str = "utf-8", verbose
                     # if verbose:
                     #     print(f"Highlight from '{key}' already exists in destionaion, skipping.")
                     continue
-                
+
                 if page:
                     header = "## Page %s | Location %s | %s \n" % (page, loc, time)
                 else:
@@ -159,7 +166,7 @@ def write_clippings(clippings: dict, path: str, encoding: str = "utf-8", verbose
                 file.write(text.encode(encoding))
                 file.write(b"\n \n")
                 sync_counter += 1
-    
+
     print(f"Sync complete.")
     print(f"Successfuly synced {sync_counter} new highlights to {path}")
     print(f"Skipped {skip_counter} highlights already existing at {path}")
